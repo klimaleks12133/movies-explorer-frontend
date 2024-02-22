26208a903024bc39a41d45ee72ce1d0121dd8f8c
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2024-02-22 16:02:37 +0300

    feat

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,12 @@
 import Header from '../Header/Header';
-import Main from '../Main/Main';
+import Main  from '../Main/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="body">
-      <section className="hero">
-        <Header></Header>
-        <Main></Main>
-      </section>
+    <div className="app">
+      <Header></Header>
+      <Main></Main>
     </div>
   );
 }
